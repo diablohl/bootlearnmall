848e100b200c4f39d887b0c6c80ceb1b9b72d5f9
diablohl <hldiablo@hotmail.com>
2021-12-06 22:26:27 +0800

    初始搭建 配置

---
+++ src/main/java/com/hl/bootlearnmall/BootlearnmallApplication.java
@@ -0,0 +1,13 @@
+package com.hl.bootlearnmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootlearnmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootlearnmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hl/bootlearnmall/BootlearnmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hl.bootlearnmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootlearnmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
